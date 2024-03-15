d07d55ee2b97205bfc90b8053264a183b4098402
Matt Kramer <matthew123003@gmail.com>
2024-03-15 16:27:52 -0400

    Classroom more done

--- src/main/java/io/zipcoder/Classroom.java
+++ src/main/java/io/zipcoder/Classroom.java
@@ -1,27 +1,36 @@
 package io.zipcoder;
 
+//import jdk.internal.org.objectweb.asm.tree.InsnList;
+
 import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.List;
 
 public class Classroom {
 
-    private Student[] students;
+    private ArrayList<Student> students;
 
     private int maxNumberOfStudents;
 
-    public Classroom(int maxNumberOfStudents){
+    public Classroom(int maxNumberOfStudents){//new Classroom with max class size
+        this.students = new ArrayList<>(maxNumberOfStudents);
         this.maxNumberOfStudents = maxNumberOfStudents;
     }
 
     public Classroom(Student[] students){
-        this.students = students;
+        this.students = new ArrayList<>(List.of(students));
     }
 
-    public Classroom(){
-        this.students = new Student[30];
+    public Classroom(){//Instantiates new classroom
+        this.students = new ArrayList<>(30);
+        this.maxNumberOfStudents = 30;
     }
 
     public Student[] getStudents() {
-        return students;
+        return this.students.toArray(new Student[0]);
+    }
+
+    public void addStudent(Student student){
+        students.add(student);
     }
 }
--- src/test/java/io/zipcoder/ClassroomTest.java
+++ src/test/java/io/zipcoder/ClassroomTest.java
@@ -1,22 +1,63 @@
 package io.zipcoder;
 
+import org.junit.Assert;
 import org.junit.Test;
 
-import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class ClassroomTest {
 
     @Test
     public void getStudentTest(){
         //Given
-        Student s1 = new Student("Matt", null, null);
-        Student s2 = new Student("Anthony", null,null);
-        Student s3 = new Student("Andy", null, null);
-        Student[] students = {s1, s2, s3};
-        String expected = "Matt, Anthony, Andy";
+        ArrayList<Double> scores = new ArrayList<>(List.of(100.0));
+        Student s1 = new Student("Matt", "Kramer", scores);
+        Student[] students = {s1};
+        String expected = "["+ s1 +"]";
         Classroom cm = new Classroom(students);
 
         //When
         Student[] actual = cm.getStudents();
+
+        //Then
+        Assert.assertEquals(expected, Arrays.toString(actual));
+    }
+
+    @Test
+    public void testGetAverageExamScore(){
+        //Given
+        ArrayList<Double> scores = new ArrayList<>(List.of(100.0, 90.0));
+        Student s1 = new Student("Matt", "Kramer", scores);
+        Student[] students = {s1};
+        double expected = 95.0;
+        Classroom cm = new Classroom(students);
+
+        //When
+        double actual = s1.getAverageExamScore();
+
+        //Then
+        Assert.assertEquals(expected, actual, 0.1);
+
+    }
+
+    @Test
+    public void testAddStudent(){
+        // : Given
+        int maxNumberOfStudents = 1;
+        Classroom classroom = new Classroom(maxNumberOfStudents);
+        ArrayList<Double> examScores  = new ArrayList<>(List.of(100.0));
+        Student student = new Student("Leon", "Hunter", examScores);
+        int expected = 1;
+
+        // When
+        classroom.addStudent(student);
+        Student[] students = classroom.getStudents();
+        int actual = students.length;
+
+        // Then
+        Assert.assertEquals(expected, actual);
+
     }
 }
