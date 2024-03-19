359142a87dfac942f330d8a807160643a8dde822
Matt Kramer <matthew123003@gmail.com>
2024-03-19 13:50:19 -0400

    close

--- src/test/java/io/zipcoder/ClassroomTest.java
+++ src/test/java/io/zipcoder/ClassroomTest.java
@@ -60,4 +60,25 @@ public class ClassroomTest {
         Assert.assertEquals(expected, actual);
 
     }
+
+    @Test
+    public void testRemoveStudent(){
+        // : Given
+        int maxNumberOfStudents = 2;
+        Classroom classroom = new Classroom(maxNumberOfStudents);
+        ArrayList<Double> examScores = new ArrayList<>(List.of(100.0, 90.0));
+        Student student = new Student("Leon", "Hunter", examScores);
+        Student student1 = new Student("Peter", "parker", examScores);
+        Integer expected = 1;
+
+        // When
+        classroom.addStudent(student);
+        classroom.addStudent(student1);
+        classroom.removeStudent("Peter", "Parker");
+        Student[] afterAdd = classroom.getStudents();
+        Integer actual = afterAdd.length;
+
+        // Then
+        Assert.assertEquals(expected, actual);
+    }
 }
